d0c20d58a2327fa368b3a14c16d57e8594443de3
AMC77 <cursosatope@gmail.com>
2020-03-09 21:01:10 +0100

    ya está bien por hoy

---
+++ app/src/main/java/com/example/agendacontrol/formularioEmpresa.java
@@ -0,0 +1,14 @@
+package com.example.agendacontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formularioEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_empresa);
+    }
+}
